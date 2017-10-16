541a9dddf6a6bee45b2e92c74c2a72ed5c399310
archiewyq <1227254255@qq.com>
2017-10-16 19:26:16 +0800

    PDFMiner3K test

--- pdf/pdfminer3kTest.py
+++ pdf/pdfminer3kTest.py
@@ -5,3 +5,20 @@ from pdfminer.layout import LAParams
 from io import StringIO
 from io import open
 
+def readPDF(pdfFile):
+    resManager = PDFResourceManager()
+    retstr = StringIO()
+    laparams = LAParams()
+    device = TextConverter(resManager, retstr, laparams=laparams)
+
+    process_pdf(resManager, device, pdfFile)
+    device.close()
+
+    content = retstr.getvalue()
+    retstr.close()
+    return content
+
+pdfFile = urlopen('http://pythonscraping.com/pages/warandpeace/chapter1.pdf')
+outputString = readPDF(pdfFile)
+print(outputString)
+pdfFile.close()
